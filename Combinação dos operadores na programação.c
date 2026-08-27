8a1530ec038cee996c44959b219b7156d59fba24
ronaldoasouzadev-lab <ronaldo.a.souza.dev@gmail.com>
2026-08-27 17:39:14 -0300

    complete

--- Combinação dos operadores na programação.c
+++ Combinação dos operadores na programação.c
@@ -9,7 +9,8 @@ void limpar_entrada() {
 
 int main() {
 
-    int numero1, numero2, numero3;
+    int numero1, numero2, numero3, soma, multiplicacao, subtracao;
+    float divisao;
 
     printf("Digite o primeiro numero: ");
     scanf("%d", &numero1);
@@ -23,11 +24,43 @@ int main() {
     scanf("%d", &numero3);
     limpar_entrada();
 
+    soma = numero1 + numero2 + numero3;
+    multiplicacao = numero1 * numero2 * numero3;
+    subtracao = numero1 - numero2 - numero3;
+
+    printf("Calculos:\n");
+    printf("\n");
+    if (numero2 == 0 || numero3 == 0) {
+        printf("Soma: %d\n", soma);
+        printf("Multiplicao: %d\n", multiplicacao);
+        printf("Subtracao: %d\n", subtracao);
+        printf("Erro: Divisao por zero nao e permitida.\n");
+    }
+    else {
+
+        divisao = (float)numero1 / (float)numero2 / (float)numero3;
+
+        printf("Soma: %d\n", soma);
+        printf("Multiplicao: %d\n", multiplicacao);
+        printf("Subtracao: %d\n", subtracao);
+        printf("Divisao: %.2f\n\n", divisao);
+
+    }
+
+    printf("Comparacoes:\n");
+     printf("\n");
+    if (numero1 > numero2 && numero2 < numero3) {
+        printf("O primeiro numero e maior que o segundo e o segundo e menor que o terceiro.\n");
+    } else {
+        printf("A condicao nao foi atendida.\n");
+    }
+
+    if (numero1 > 0 && numero2 % 2 == 0) {
+        printf("O primeiro numero e positivo e o segundo e par.\n");
+    }
+    else {
+        printf("O primeiro numero nao e positivo e o segundo nao e par.\n");
+    }
     
-
-
-    
-
-
     return 0;
 }
